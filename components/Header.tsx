6cf7c6daa8afbeec09009c7b59084ba655a61ecf
emorilebo <emorylebo@gmail.com>
2022-01-21 19:47:46 +0100

    Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 function Header() {
   return (
     <header>
       <div>
+        <Link href="/">
+          <img src="https://links.papareact.com" alt="" />
+        </Link>
+
         <h1>Medium Blog</h1>
       </div>
       <div></div>
