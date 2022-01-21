5e48f414f82559896f4251d06eecbf76c99baab4
emorilebo <emorylebo@gmail.com>
2022-01-21 19:27:13 +0100

    ADD Header

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <h1>Medium Blog</h1>
+      </div>
+      <div></div>
+    </header>
+  );
+}
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Curious Medium</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     
+      <Header />
     </div>
-  )
+  );
 }
