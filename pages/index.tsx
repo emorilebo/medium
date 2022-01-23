40aedcc5028759eaf057c3f5ab6160c25c75043c
emorilebo <emorylebo@gmail.com>
2022-01-23 10:27:59 +0100

    UPDATE Banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,13 @@ export default function Home() {
 
       <Header />
       <div>
-        <div>
-          <h1>CuriousityExploring is a place tto write, read and connect</h1>
+        <div className="px-10 space-y-5">
+          <h1 className="text-6xl max-w-xl font-serif">
+            <span className="underline decoration-black decoration-4">
+              CuriousityExploring
+            </span>{" "}
+            is a place to write, read and connect
+          </h1>
           <h2>
             It is easy and free to post your thinking on any topic and connect
             with millions of folks
