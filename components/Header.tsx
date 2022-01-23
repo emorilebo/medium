4f7cbc47b9217e2173453284e0754e54e8ea4ec9
emorilebo <emorylebo@gmail.com>
2022-01-23 08:49:11 +0100

    UPDATE Follow

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <div className="flex">
+      <div className="flex items-center space-x-5">
         <Link href="/">
           <img
             className="w-44 object-contain cursor-pointer"
@@ -16,7 +16,7 @@ function Header() {
         <div className="hidden md:inline-flex items-center space-x-5">
           <h3>About</h3>
           <h3>Contact</h3>
-          <h3>Follow</h3>
+          <h3 className="text-white bg-green-500 px-4 py-1 rounded-full">Follow</h3>
         </div>
       </div>
       <div></div>
