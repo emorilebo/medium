3fb65cac4cd64c576d9b93e4928c47a358a2a973
emorilebo <emorylebo@gmail.com>
2022-01-23 09:38:15 +0100

    UPDATE Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Header() {
   return (
-    <header>
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
       <div className="flex items-center space-x-5">
         <Link href="/">
           <img
@@ -16,10 +16,17 @@ function Header() {
         <div className="hidden md:inline-flex items-center space-x-5">
           <h3>About</h3>
           <h3>Contact</h3>
-          <h3 className="text-white bg-green-500 px-4 py-1 rounded-full">Follow</h3>
+          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">
+            Follow
+          </h3>
         </div>
       </div>
-      <div></div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign In</h3>
+        <h3 className="border px-4 py-1 rounded-full border-green-600">
+          Get Started
+        </h3>
+      </div>
     </header>
   );
 }
