bb7b00c276dcb2efae6f66aed3cffe6f37de51de
emorilebo <emorylebo@gmail.com>
2022-01-24 11:15:39 +0100

    ADD sanityClient

--- sanity.js
+++ sanity.js
@@ -20,3 +20,6 @@ export const config = {
    */
   useCdn: process.env.NODE_ENV == "production",
 };
+
+//Set up the client for fetching data in the getProps page functions
+export const sanityClient = createClient(config);
