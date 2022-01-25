f46401693e4ba372ec3140447b21bac195423d02
emorilebo <emorylebo@gmail.com>
2022-01-25 18:28:43 +0100

    ADD responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,14 +39,14 @@ export default function Home({ posts }: Props) {
         />
       </div>
       {/* post */}
-      <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
         {posts.map((post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div>
+            <div className="">
               <img src={urlFor(post.mainImage).url()!} alt="" />
-              <div>
+              <div className="flex justify-between p-5 bg-white">
                 <div>
-                  <p>{post.title}</p>
+                  <p className="">{post.title}</p>
                   <p>{post.description} by {post.author.name}</p>
                   
                 </div>
