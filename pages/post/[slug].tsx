9eeb92f1267c11874088f10149c5a0324c334f16
emorilebo <emorylebo@gmail.com>
2022-01-26 16:56:03 +0100

    UPDATE  getStaticProps

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -35,5 +35,23 @@ export const getStaticPaths = async () => {
 
 
 export const getStaticProps: GetStaticProps = async ({params})=>{
+  const query = `*[_type == "post" && slug.current == $slug][0]{
+    _id,
+    _createdAt,
+    title,
+    author->{
+    name,
+    image
+  },
+  'comments': *[
+    _type == "comment" &&
+    post.ref == ^._id &&
+    approved == true],
+  description,
+  mainImage,
+  slug,
+  body
+  }  `;
+
   
 }
