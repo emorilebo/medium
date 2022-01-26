37af32635c9b107bbced37b2dfe634a03105f89b
emorilebo <emorylebo@gmail.com>
2022-01-26 15:00:28 +0100

    UPDATE slug

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -12,14 +12,10 @@ function Post() {
 export default Post;
 
 export const getStaticPaths = async () => {
-  //const query
-  /**
-     * The loss of a mother is unimaginable, and there are no words to find consolation in.
-
-Yet she lived well Sir, you be a living proof to that, and so our loss is, in fact, her gain; because she is with the Almighty now. 
-
-I hope you find consolation in that Sir, and may the mother of our Lord Jesus whose brings all hope, bring you Strength to bear this loss.
-
-Accept my deepest condolence Sir Roland
-     */
+  const query = `*[_type == "post"]{
+    _id,
+    slug{
+    current
+  }
+  }`;
 };
