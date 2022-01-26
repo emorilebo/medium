396aba752e39ad6a7c7087092c58ebc76afe4f9d
emorilebo <emorylebo@gmail.com>
2022-01-26 14:49:43 +0100

    UPDATE slug

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -12,5 +12,14 @@ function Post() {
 export default Post;
 
 export const getStaticPaths = async () => {
-    //const query 
+  //const query
+  /**
+     * The loss of a mother is unimaginable, and there are no words to find consolation in.
+
+Yet she lived well Sir, you be a living proof to that, and so our loss is, in fact, her gain; because she is with the Almighty now. 
+
+I hope you find consolation in that Sir, and may the mother of our Lord Jesus whose brings all hope, bring you Strength to bear this loss.
+
+Accept my deepest condolence Sir Roland
+     */
 };
