e3d0dee3e50546663421583263a60540f91204eb
emorilebo <emorylebo@gmail.com>
2022-01-27 07:18:47 +0100

    UPDATE  getStaticProps revalidate

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -71,5 +71,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       post,
     },
+    revalidate: 60, //after 60 seconds update the old cached version
   };
 };
