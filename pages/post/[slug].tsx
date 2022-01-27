64e2d754d5c4f948d466d5f3a0750b8990ce5604
emorilebo <emorylebo@gmail.com>
2022-01-27 13:58:48 +0100

    UPDATE comment with validation error

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -108,6 +108,18 @@ function Post({ post }: Props) {
             rows={8}
           />
         </label>
+        {/* erros will return when field  validation fails */}
+        <div className="flex flex-col p-5">
+          {errors.name && (
+            <span className="text-red-500"> - The Name Field is required</span>
+          )}
+          {errors.comment && (
+            <span className="text-red-500"> - The Comment Field is required</span>
+          )}
+          {errors.email && (
+            <span className="text-red-500"> - The Email Field is required</span>
+          )}
+        </div>
       </form>
     </main>
   );
