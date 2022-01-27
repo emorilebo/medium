a9a4151abd01cd3c4bcf5f24211401925c6bdacf
emorilebo <emorylebo@gmail.com>
2022-01-27 11:32:36 +0100

    ADD comment

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -61,7 +61,10 @@ function Post({ post }: Props) {
       </article>
       <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
 
-      <form>
+      <form className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
+        <h3>Enjoyed this article?</h3>
+        <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+        <hr className="py-3 mt-2" />
         <label>
           <span>Name</span>
           <input placeholder="Francis Ekpen" type="text" />
@@ -74,7 +77,6 @@ function Post({ post }: Props) {
           <span>Comment</span>
           <textarea placeholder="What's on your mind?" rows={8} />
         </label>
-       
       </form>
     </main>
   );
