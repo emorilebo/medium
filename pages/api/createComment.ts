d58a344bc573dedff1eb7a86eda8d15d1a0df753
emorilebo <emorylebo@gmail.com>
2022-01-27 15:23:29 +0100

    UPDATE createComment

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -11,9 +11,24 @@ const config = {
 
 const client = sanityClient(config)
 
-export default function createComment(
+export default async function createComment(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+    const {_id, name, email, comment} = JSON.parse(req.body);
+
+    try{
+        await client.create({
+            _type: 'comment',
+            post: {
+                _type: 'reference',
+                _ref: _id
+            },
+            name,
+            email,
+            comment
+            
+        })
+    }
   res.status(200).json({ name: 'John Doe' })
 }
