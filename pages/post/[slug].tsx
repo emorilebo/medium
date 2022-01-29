09f9a7283620e55fcaeda74e5f612ee085b0d401
emorilebo <emorylebo@gmail.com>
2022-01-29 16:59:43 +0100

    UPDATE comment with conditional rendering of form

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -91,63 +91,78 @@ function Post({ post }: Props) {
       </article>
       <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
 
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col p-5 max-w-2xl mx-auto mb-10"
-      >
-        <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
-        <h4 className="text-3xl font-bold">Leave a comment below!</h4>
-        <hr className="py-3 mt-2" />
+      {submitted ? (
+        <h1>Submitted</h1>
+      ) : (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col p-5 max-w-2xl mx-auto mb-10"
+        >
+          <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+          <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+          <hr className="py-3 mt-2" />
 
-        <input {...register("_id")} type="hidden" name="_id" value={post._id} />
+          <input
+            {...register("_id")}
+            type="hidden"
+            name="_id"
+            value={post._id}
+          />
 
-        <label className="block mb-5">
-          <span className="text-gray-700">Name</span>
+          <label className="block mb-5">
+            <span className="text-gray-700">Name</span>
+            <input
+              {...register("name", { required: true })}
+              className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+              placeholder="Francis Ekpen"
+              type="text"
+            />
+          </label>
+          <label className="block mb-5">
+            <span className="text-gray-700">Email</span>
+            <input
+              {...register("email", { required: true })}
+              className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+              placeholder="francis@gmail.com"
+              type="email"
+            />
+          </label>
+          <label className="block mb-5">
+            <span className="text-gray-700">Comment</span>
+            <textarea
+              {...register("comment", { required: true })}
+              className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+              placeholder="What's on your mind?"
+              rows={8}
+            />
+          </label>
+          {/* erros will return when field  validation fails */}
+          <div className="flex flex-col p-5">
+            {errors.name && (
+              <span className="text-red-500">
+                {" "}
+                - The Name Field is required
+              </span>
+            )}
+            {errors.comment && (
+              <span className="text-red-500">
+                {" "}
+                - The Comment Field is required
+              </span>
+            )}
+            {errors.email && (
+              <span className="text-red-500">
+                {" "}
+                - The Email Field is required
+              </span>
+            )}
+          </div>
           <input
-            {...register("name", { required: true })}
-            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
-            placeholder="Francis Ekpen"
-            type="text"
+            type="submit"
+            className="shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer"
           />
-        </label>
-        <label className="block mb-5">
-          <span className="text-gray-700">Email</span>
-          <input
-            {...register("email", { required: true })}
-            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
-            placeholder="francis@gmail.com"
-            type="email"
-          />
-        </label>
-        <label className="block mb-5">
-          <span className="text-gray-700">Comment</span>
-          <textarea
-            {...register("comment", { required: true })}
-            className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring"
-            placeholder="What's on your mind?"
-            rows={8}
-          />
-        </label>
-        {/* erros will return when field  validation fails */}
-        <div className="flex flex-col p-5">
-          {errors.name && (
-            <span className="text-red-500"> - The Name Field is required</span>
-          )}
-          {errors.comment && (
-            <span className="text-red-500">
-              {" "}
-              - The Comment Field is required
-            </span>
-          )}
-          {errors.email && (
-            <span className="text-red-500"> - The Email Field is required</span>
-          )}
-        </div>
-        <input
-          type="submit"
-          className="shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer"
-        />
-      </form>
+        </form>
+      )}
     </main>
   );
 }
